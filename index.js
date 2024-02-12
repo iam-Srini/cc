77b4f10234d897c222a9f0afe1f38cd093525cde
iam-Srini <151881616+iam-Srini@users.noreply.github.com>
2024-02-12 00:57:49 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+
+// Create a div element
+const container = document.createElement("div");
+
+// Set the innerHTML of the div with the provided HTML content
+container.innerHTML = `
+    <h1> Marri Srinivas Reddy </h1>
+    <p>4973</p>
+    <p>Heroku</p>
+    <p>11 Feb 2024</p>
+    <h2>Simple Static Website Assignment Spring 2024 using Cloud Managed Web Service</h2>
+`;
+
+// Append the div to the body of the document
+document.body.appendChild(container);
